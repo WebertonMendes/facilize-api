0199b48bfbd0a37282be02536899cdc2c5af4b49
weberton.mendes <weberton.mendes@cortex-intelligence.com>
2023-05-23 00:53:48 -0300

    Alternate database to sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,23 +9,13 @@ import { AuthModule } from './modules/auth/auth.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      // envFilePath: resolve(`${__dirname}/.env`),
       isGlobal: true,
     }),
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.TYPEORM_HOST,
-      port: parseInt(process.env.TYPEORM_PORT),
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      entities: [process.env.TYPEORM_ENTITIES],
-      synchronize: false,
-      // extra: {
-      //   ssl: {
-      //     rejectUnauthorized: false
-      //   }
-      // }
+      type: 'sqlite',
+      database: 'src/database/sqlite/db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
     }),
     AuthModule,
     TasksModule,
