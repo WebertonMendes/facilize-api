a2554f3c60189ceac05d1d6922778f775db2eb6a
Weberton Mendes <weberton.mendes@cortex-intelligence.com>
2023-12-13 01:52:38 -0300

    cors config vercel domain

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,30 @@ import { AppModule } from './app.module';
 import { EntityNotFoundExceptionFilter } from './errors/entity-not-found.exception';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalFilters(new EntityNotFoundExceptionFilter());
+
+  app.enableCors({
+    origin: [process.env.DOMAIN_URL],
+    methods: ['POST', 'PATCH', 'DELETE', 'GET']
+  });
 
   const swaggerOptions = new DocumentBuilder()
     .setTitle('To-Do List API')
     .setDescription('API Documentation "To-Do List"')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
 
   const swaggerDocument = SwaggerModule.createDocument(app, swaggerOptions);
   SwaggerModule.setup('api/v1/docs', app, swaggerDocument);
 
-  await app.listen(process.env.APP_PORT || process.env.PORT || 3333);
+  const applicationPort = process.env.APP_PORT || process.env.PORT || 3333;
 
-  console.log(`🚀 API: http://localhost:${process.env.APP_PORT}/api/v1/`);
-  console.log(`📝 DOC: http://localhost:${process.env.APP_PORT}/api/v1/docs`);
+  await app.listen(applicationPort);
+
+  console.log(`🚀 API: http://localhost:${applicationPort}/api/v1/`);
+  console.log(`📝 DOC: http://localhost:${applicationPort}/api/v1/docs`);
 }
 bootstrap();
